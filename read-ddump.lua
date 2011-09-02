89db34d5835a672d303586603c9479692250fd8b
Charles Stewart <charles.alexander.stewart@gmail.com>
2011-09-02 07:47:32 +0200

    Working html output, ugly as sin.  Should impose order on comments table.

--- read-ddump.lua
+++ read-ddump.lua
@@ -3,6 +3,25 @@
 -- Some functions that can be used when reading in the Lua rep'n of SX data dumps
 -- as output by sxdatadump2lua
 
+-- -- Some general-purpose definitions
+nl = "\n"
+q = function (string) return "\"" .. tostring(string) .. "\"" end
+
+function map (f, table)
+	local r = {}
+	for key,val in ipairs (table) do
+	    	r[key] = f(val)
+	end
+	return r
+end    
+
+function log (...) print ("log:", ...) end
+-- function log (...) end
+
+
+-- -- Initialise main data-carrying variables
+
+site = {title="data dump"}
 users = {type="users"}
 posts = {type="posts"}; questions = {}
 comments = {type="comments"}
@@ -17,7 +36,7 @@ function unquote_sx (qtext)
 	-- Referenced in type_trans_fwd: fn which makes strings legible.
 	-- The SX data dumps makes characters like <, &, and < safe by mapping
 	-- to their HTML character entities.  This fn maps them back.
-	return (string.gsub (qtext, "&.*;", unsxquotations))
+	return (string.gsub (qtext, "&%a+;", unsxquotations))
 end
 
 function follow_index_fn (tlt)
@@ -42,7 +61,7 @@ type_trans_fwd = {
 	AcceptedAnswerId = follow_post_index, -- Used only by questions
 	PostId = follow_post_index, -- Used by comments, posthistory, and votes
 	UserId = follow_index_fn (users), -- Used by badges and posthistory
-	Body=unquote_sx, Title=unquote_sx, 
+	Body=unquote_sx, -- Title=unquote_sx, 
 	AboutMe=unquote_sx,
 	Text=unquote_sx }
 
@@ -64,7 +83,7 @@ function add_row (tlt, trans, row)
 	return tlt[i]
 end
 
--- Now come the functions that interpret the Lua-ised data dump.
+-- -- Now come the semantics of the Lua-ised data dump.
 
 function user (row)
 	add_row (users, type_trans_fwd, row)
